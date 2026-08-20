088fd88c0d182d4cf833ad3e587681f34293a760
Abood059 <ah0597414132@gmail.com>
2026-08-20 13:04:15 +0300

    fix: simplify api entry, rely on tsc-alias with tsconfig.build.json

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,3 @@
 // api/index.ts
-const moduleAlias = require('module-alias');
-const path = require('path');
-
-moduleAlias.addAliases({
-  '@': path.join(__dirname, '..', 'dist'),
-  '@common': path.join(__dirname, '..', 'dist/common'),
-  '@modules': path.join(__dirname, '..', 'dist/modules'),
-  '@config': path.join(__dirname, '..', 'dist/config'),
-});
-
-module.exports = require('../dist/main');
+// @ts-ignore - dist/main.js will exist after build
+export { default } from '../dist/main';
